12b2d4dcbf2eedde8cef87c287e9a8e5ad8c4107
Arcadius Kazimierski <a@Arcadiuss-MacBook-Pro.local>
2015-01-12 17:35:37 -0500

    added cart endpoint and fixed order endpoint id types

---
+++ server/api/cart/cart.model.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CartSchema = new Schema({
+  userId: [{ type: Schema.Types.ObjectId, ref: 'user', default: null }], // null or undefined if user not logged in
+  cartCreationDate: Date,
+  productIdArray: Array,
+  productQtyArray: Array
+});
+
+module.exports = mongoose.model('Cart', CartSchema);
--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/cart/cart.socket').register(socket);
   require('../api/orders/orders.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
