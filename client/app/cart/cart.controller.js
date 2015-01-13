d300bb64c438953e2a55806fde92bc6e6098431f
Cifer9 <christian.kara90@gmail.com>
2015-01-13 18:45:06 -0500

    Pretty cart in progress

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,8 +1,20 @@
 'use strict';
 
 angular.module('stackstoreApp')
-  .controller('CartCtrl', function ($scope) {
+  .controller('CartCtrl', function ($scope, Product, $http) {
     // $scope.message = 'Hello';
-    $scope.cart = {}
+    //$scope.products = Product.cart();
 
+    Product.cart().then(function(data){
+    	$scope.products=data;
+    });
+
+    console.log($scope.products);
+    //$http.get('/api/carts').success(function(cartData){
+    	// console.log(cartData)
+    	// return cartData
+   // })
+    // $scope.products=cartData
   });
+
+
